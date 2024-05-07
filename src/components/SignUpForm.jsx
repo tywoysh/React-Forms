5ecf12d9605068dc088f4cc087c5e09153d786a8
Tyler Woyshner <tylerwoyshner@Tylers-MacBook-Pro.local>
2024-05-06 21:26:29 -0400

    fetch api

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event){
+    event.preventDefault();
+    try {
+        const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup',
+        { 
+            method: "POST", 
+            headers: { 
+              "Content-Type": "application/json" 
+            }, 
+            body: JSON.stringify({ 
+              username: {username}, 
+              password: {password} 
+            }) 
+          })
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        setError(error.message);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <label>
+        Username: <input value={username} onChange={(event)=>setUsername(event.target.value)}/>
+      </label>
+      <label>
+        Password: <input type="password" value={password} onChange={(event)=>setPassword(event.target.value)}/>
+      </label>
+      <button>Submit</button>
+    </form>
+  );
+}
